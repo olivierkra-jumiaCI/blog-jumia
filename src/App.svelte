63d4fa5e5e3433e8362ab18dfb75914335540bea
olivierkra-jumiaCI <olivier.kra@jumia.com>
2026-05-05 18:12:20 +0000

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -11,10 +11,12 @@
 <JumiaBlog onNavigate={handleNavigate} />
 
 <style>
-  :global(body) {
-    margin: 0;
+  :global(html), :global(body) {
+    margin: 0 auto;
     padding: 0;
     font-family: 'Open Sans', Arial, sans-serif;
     background-color: #f5f5f5;
+    max-width: 1170px;
+    overflow-x: hidden;
   }
 </style>
