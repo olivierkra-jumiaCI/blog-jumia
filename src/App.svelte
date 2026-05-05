57e671068ac15f637e7bdc3562d22d4d20e05c13
olivierkra-jumiaCI <olivier.kra@jumia.com>
2026-05-05 17:19:44 +0000

    Initial commit: Jumia Blog Svelte project

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import JumiaBlog from './JumiaBlog.svelte';
+
+  let currentPath = '/blog/';
+
+  function handleNavigate(path) {
+    currentPath = path;
+  }
+</script>
+
+<JumiaBlog onNavigate={handleNavigate} />
+
+<style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    font-family: 'Open Sans', Arial, sans-serif;
+    background-color: #f5f5f5;
+  }
+</style>
